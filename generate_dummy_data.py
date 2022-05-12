13379e740d6b977c0f6bf4cdee2dcafac3988059
BrianWong05 <wongtiniaomacau@gmail.com>
2022-05-12 17:07:24 +0800

    move to algo

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -6,13 +6,13 @@ from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import server
+import app
 
 # app
 app = Flask(__name__)
 
 # config
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
 # configure sqlite3 to enforce foreign key contraints
@@ -35,7 +35,7 @@ for i in range(200):
     address = faker.address()
     phone = faker.msisdn()
     email = f'{name.replace(" ", "_")}@email.com'
-    new_user = server.User(name=name, address=address, phone=phone, email=email)
+    new_user = app.User(name=name, address=address, phone=phone, email=email)
     db.session.add(new_user)
     db.session.commit()
 
@@ -46,7 +46,7 @@ for i in range(200):
     date = faker.date_time()
     user_id = randrange(1, 200)
 
-    new_blog_post = server.BlogPost(
+    new_blog_post = app.BlogPost(
         title=title, body=body, date=date, user_id=user_id
     )
     db.session.add(new_blog_post)
